fc334616546d91698ff4b1ac1c70e837c6a843d4
Eric Zhao <21zhaoe@protonmail.com>
2021-06-29 11:13:34 -0400

    fix: Fix iterator implementations

--- src/iter.rs
+++ src/iter.rs
@@ -5,25 +5,22 @@ use crate::Lobby;
 /// Immutable Lobby iterator.
 #[derive(Debug)]
 pub struct Iter<'a, T, const N: usize> {
-    inner: &'a Lobby<T, N>,
-
-    idx: usize,
+    inner: IterFull<'a, T, N>,
 }
 
 impl<'a, T, const N: usize> Iter<'a, T, N> {
     /// Create a new Iter.
     #[inline]
     pub fn new(inner: &'a Lobby<T, N>) -> Self {
-        Self { inner, idx: 0 }
+        Self {
+            inner: IterFull { inner, idx: 0 },
+        }
     }
 
     /// Transforms this iterator into an `IterFull`.
     #[inline]
     pub fn with_full(self) -> IterFull<'a, T, N> {
-        IterFull {
-            inner: self.inner,
-            idx: self.idx,
-        }
+        self.inner
     }
 }
 
@@ -32,12 +29,7 @@ impl<'a, T, const N: usize> Iterator for Iter<'a, T, N> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.idx += 1;
-        if self.idx > N {
-            None
-        } else {
-            self.inner.nth(self.idx - 1)
-        }
+        self.inner.next().and_then(|v| v)
     }
 }
 
@@ -66,9 +58,7 @@ impl<'a, T, const N: usize> Iterator for IterFull<'a, T, N> {
 /// Mutable Lobby iterator.
 #[derive(Debug)]
 pub struct IterMut<'a, T, const N: usize> {
-    inner: &'a mut [Option<T>; N],
-
-    idx: usize,
+    inner: IterMutFull<'a, T, N>,
 }
 
 impl<'a, T, const N: usize> IterMut<'a, T, N> {
@@ -76,20 +66,39 @@ impl<'a, T, const N: usize> IterMut<'a, T, N> {
     #[inline]
     pub fn new(inner: &'a mut Lobby<T, N>) -> Self {
         Self {
-            inner: &mut inner.arr,
-            idx: 0,
+            inner: IterMutFull {
+                inner: &mut inner.arr,
+                idx: 0,
+            },
         }
     }
 
     /// Transforms this iterator into an [`IterMutFull`].
     #[inline]
     pub fn with_full(self) -> IterMutFull<'a, T, N> {
-        IterMutFull { iter: self }
+        self.inner
     }
 }
 
 impl<'a, T, const N: usize> Iterator for IterMut<'a, T, N> {
     type Item = &'a mut T;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().and_then(|v| v)
+    }
+}
+
+/// Mutable Lobby iterator that yields `N` items for each space in the Lobby.
+#[derive(Debug)]
+pub struct IterMutFull<'a, T, const N: usize> {
+    inner: &'a mut [Option<T>; N],
+
+    idx: usize,
+}
+
+impl<'a, T, const N: usize> Iterator for IterMutFull<'a, T, N> {
+    type Item = Option<&'a mut T>;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
@@ -99,43 +108,30 @@ impl<'a, T, const N: usize> Iterator for IterMut<'a, T, N> {
             // SAFETY: self.idx is guaranteed to be within bounds.
             let v = (unsafe { &mut *self.inner.as_mut_ptr().add(self.idx) }).as_mut();
             self.idx += 1;
-            v
+            Some(v)
         }
-    }
-}
-
-/// Mutable Lobby iterator that yields `N` items for each space in the Lobby.
-#[derive(Debug)]
-pub struct IterMutFull<'a, T, const N: usize> {
-    iter: IterMut<'a, T, N>,
-}
-
-impl<'a, T, const N: usize> Iterator for IterMutFull<'a, T, N> {
-    type Item = Option<&'a mut T>;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(Some)
     }
 }
 
 /// Owning Lobby iterator.
 #[derive(Debug)]
 pub struct IntoIter<T, const N: usize> {
-    inner: Lobby<T, N>,
+    inner: IntoIterFull<T, N>,
 }
 
 impl<'a, T, const N: usize> IntoIter<T, N> {
     /// Create a new IterMut.
     #[inline]
     pub fn new(inner: Lobby<T, N>) -> Self {
-        Self { inner }
+        Self {
+            inner: IntoIterFull { inner, idx: 0 },
+        }
     }
 
     /// Transforms this iterator into an [`IntoIterFull`].
     #[inline]
     pub fn with_full(self) -> IntoIterFull<T, N> {
-        IntoIterFull { inner: self.inner }
+        self.inner
     }
 }
 
@@ -144,7 +140,7 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.shift()
+        self.inner.next().and_then(|v| v)
     }
 }
 
@@ -152,12 +148,20 @@ impl<T, const N: usize> Iterator for IntoIter<T, N> {
 #[derive(Debug)]
 pub struct IntoIterFull<T, const N: usize> {
     inner: Lobby<T, N>,
+
+    idx: usize,
 }
 
 impl<T, const N: usize> Iterator for IntoIterFull<T, N> {
     type Item = Option<T>;
 
+    #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.inner.shift())
+        if self.idx >= N {
+            None
+        } else {
+            self.idx += 1;
+            Some(self.inner.shift())
+        }
     }
 }
